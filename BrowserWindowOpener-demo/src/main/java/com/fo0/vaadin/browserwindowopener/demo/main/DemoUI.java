2a117d96430058af85c834fee788f87d3525920a
fo0 <fo0systemsinfo@gmail.com>
2018-02-07 19:36:52 +0100

    added demo

--- BrowserWindowOpener-demo/src/main/java/com/fo0/vaadin/browserwindowopener/demo/main/DemoUI.java
+++ BrowserWindowOpener-demo/src/main/java/com/fo0/vaadin/browserwindowopener/demo/main/DemoUI.java
@@ -2,13 +2,16 @@ package com.fo0.vaadin.browserwindowopener.demo.main;
 
 import javax.servlet.annotation.WebServlet;
 
+import org.vaadin.viritin.label.MLabel;
+import org.vaadin.viritin.layouts.MVerticalLayout;
+
 import com.fo0.vaadin.browserwindowopener.main.PopupConfiguration;
 import com.fo0.vaadin.browserwindowopener.main.WindowOpenerButton;
 import com.vaadin.annotations.Push;
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.Title;
 import com.vaadin.annotations.VaadinServletConfiguration;
-import com.vaadin.annotations.Widgetset;
+import com.vaadin.icons.VaadinIcons;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
 import com.vaadin.shared.communication.PushMode;
@@ -27,7 +30,17 @@ public class DemoUI extends UI {
 
 	@Override
 	protected void init(VaadinRequest request) {
+
 		WindowOpenerButton btn = new WindowOpenerButton(PopupConfiguration.builder().build(), new MyLayout());
-		setContent(btn);
+		btn.setIcon(VaadinIcons.BROWSER);
+		btn.setCaption("Custom Browser Popup");
+
+		WindowOpenerButton btn2 = new WindowOpenerButton(PopupConfiguration.builder().build(),
+				new MVerticalLayout(new MLabel("This is my content")));
+		btn2.setIcon(VaadinIcons.BROWSER);
+		btn2.setCaption("Custom Browser Popup 2");
+
+		setContent(new MVerticalLayout(
+				new MLabel("Please care, that a current bug is that your first open needs >two< clicks"), btn, btn2));
 	}
 }
